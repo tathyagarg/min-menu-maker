f88bedb323f4d68ae1f2f8301a1e21f6ddef4769
Tathya <94824143+tathyagarg@users.noreply.github.com>
2024-01-03 20:12:23 +0530

    Update menus.py

--- menus.py
+++ menus.py
@@ -1,25 +1,50 @@
-from typing import Any, Callable
+from typing import Callable
 from bullets import Bullets
 
-# This whole file is basically one big TODO
 
 class Choice:
     def __init__(
-        self, 
-        action: Callable
+        self,
+        message: str,
+        action: Callable,
+        *args,
+        **kwargs
     ) -> None:
+        self.message = message
         self.action = action
+        self.args = args
+        self.kwargs = kwargs
 
-    def __call__(self, *args: Any, **kwargs: Any) -> Any:
-        return self.action(*args, **kwargs)
+    def __call__(self):
+        return self.action(*self.args, **self.kwargs)
+
+    def __str__(self) -> str:
+        return self.message
 
 
 class Menu:
     def __init__(
         self,
-        greeting: str,
         choices: list[Choice],
-        bulleting_type: Bullets
+        bulleting_type: Bullets,
+        greeting: str = None,
+        **kwargs
     ) -> None:
-        pass
+        self.greeting = greeting
+        self.choices = choices
+        self.bulleting = bulleting_type
+
+        self.include_space = kwargs.get('include_space', True)
+        self.input_message = kwargs.get('input_message', '>>> ')
+
+    def __call__(self):
+        if self.greeting:
+            print(self.greeting)
+
+        for bullet, choice in zip(self.bulleting, self.choices):
+            print(f"{bullet}{' ' if self.include_space else ''}{choice}")
+        choice = input(self.input_message)
+        index = self.bulleting.convert(choice)
+        print(index)
+        self.choices[index]()
 
