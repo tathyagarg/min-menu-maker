623659c98127a3aca448e65bd6ba80ada52b5852
Tathya <94824143+tathyagarg@users.noreply.github.com>
2024-01-03 20:13:03 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,21 @@
 import bullets
+from menus import Menu, Choice
 
-rad = iter(bullets.Radix('turtle'))
+def beans(pwd: int):
+    if pwd == 279: print("Beans")
+    else: print("Not beans")
 
-for i in range(11196): # Prints turtle at this weird index!
-    print(next(rad))
+menu = Menu([
+    Choice("Say hi but diff lang omg op",
+        Menu([
+            Choice("German", print, "Hallo"),
+            Choice("Russian", print, "Privyet"),
+            Choice("Latin", print, "Salve")
+        ], bullets.NUMERICAL())
+    ),
+    Choice("Say hello", print, "Hello!"),
+    Choice("Bean 12", beans, 12),
+    Choice("Bean 279", beans, 279),
+], bullets.Bullets('xy', '.'), "Choose!!!")
+
+menu()
